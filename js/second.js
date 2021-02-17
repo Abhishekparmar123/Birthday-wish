a2a45b68dd1b423b2b4c0586f7617de3c9392ac1
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2021-02-17 08:18:53 +0530

    Save changes

--- js/second.js
+++ js/second.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
     hh = h / 2,
 
     opts = {
-        strings: [ 'HAPPY', 'BIRTHDAY!', 'Aashish'],
+        strings: [ 'HAPPY', 'BIRTHDAY!', 'AASHISH'],
         charSize: 30,
         charSpacing: 35,
         lineHeight: 50,
